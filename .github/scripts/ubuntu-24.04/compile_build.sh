b3519ca7726675dd23b61d9a81cf773049e8cc2d
Máté Cserép <mcserep@gmail.com>
2025-05-26 08:52:01 +0200

    Add CI for Ubuntu 24.04

---
+++ .github/scripts/ubuntu-24.04/compile_build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Compile dependencies that cannot be acquired via the official repositories
+
+mkdir -p ${INSTALL_PATH}
+
+## Compile Thrift
+
+tar -xvf ${DOWNLOAD_PATH}/thrift-0.16.0.tar.gz
+cd thrift-0.16.0
+./configure --prefix=${INSTALL_PATH}/thrift --silent --without-python \
+  --enable-libtool-lock --enable-tutorial=no --enable-tests=no      \
+  --with-libevent --with-zlib --without-nodejs --without-lua        \
+  --without-ruby --without-csharp --without-erlang --without-perl   \
+  --without-php --without-php_extension --without-dart              \
+  --without-haskell --without-go --without-rs --without-haxe        \
+  --without-dotnetcore --without-d --without-qt4 --without-qt5      \
+  --without-java --without-swift
+
+make install -j $(nproc)
+
+## Compile build2
+
+sh "${DOWNLOAD_PATH}/install_latest_build2.sh" ${INSTALL_PATH}/build2
+
+## Compile odb, libodb, and its connectors
+
+mkdir -p ${DOWNLOAD_PATH}/odb
+cd ${DOWNLOAD_PATH}/odb
+${INSTALL_PATH}/build2/bin/bpkg create --quiet --jobs $(nproc) cc \
+  config.cxx=g++ \
+  config.cc.coptions=-O3 \
+  config.bin.rpath=${INSTALL_PATH}/odb/lib \
+  config.install.root=${INSTALL_PATH}/odb
+
+### Getting the source
+${INSTALL_PATH}/build2/bin/bpkg add https://pkg.cppget.org/1/beta --trust-yes
+${INSTALL_PATH}/build2/bin/bpkg fetch --trust-yes
+
+### Building odb
+${INSTALL_PATH}/build2/bin/bpkg build odb --yes
+${INSTALL_PATH}/build2/bin/bpkg build libodb --yes
+${INSTALL_PATH}/build2/bin/bpkg build libodb-sqlite --yes
+${INSTALL_PATH}/build2/bin/bpkg build libodb-pgsql --yes
+${INSTALL_PATH}/build2/bin/bpkg install --all --recursive
--- .github/scripts/ubuntu-24.04/download_build.sh
+++ .github/scripts/ubuntu-24.04/download_build.sh
@@ -4,15 +4,29 @@
 
 mkdir -p "${DOWNLOAD_PATH}"
 
+## Thrift 0.16
+
+wget -O ${DOWNLOAD_PATH}/thrift-0.16.0.tar.gz "http://archive.apache.org/dist/thrift/0.16.0/thrift-0.16.0.tar.gz"
+
 ## ODB
 
 wget -O "${DOWNLOAD_PATH}/install_latest_build2.sh" "https://github.com/Ericsson/CodeCompass/raw/master/scripts/install_latest_build2.sh"
 build2_version=$(sh "${DOWNLOAD_PATH}/install_latest_build2.sh" --version)
 odb_signature=$(wget -qO- https://pkg.cppget.org/1/beta/signature.manifest)
+odb_hash=$(echo -n "${build2_version}${odb_signature}" | md5sum | awk '{print $1}')
 
 # Calculate hash of dependencies for Github Cache Action
 
-hash_value=$(echo -n "${build2_version}${odb_signature}" | md5sum | awk '{print $1}')
+dependencies_to_hash=("thrift-0.16.0.tar.gz")
+
+concatenated_hashes=""
+for file in "${dependencies_to_hash[@]}"; do
+    file_hash=$(md5sum "${DOWNLOAD_PATH}/${file}" | awk '{print $1}')
+    concatenated_hashes="${concatenated_hashes}${file_hash}"
+done
+concatenated_hashes="${concatenated_hashes}${odb_hash}"
+
+hash_value=$(echo -n "${concatenated_hashes}" | md5sum | awk '{print $1}')
 
 ## Save said hash
 
